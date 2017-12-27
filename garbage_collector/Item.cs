2204ecd0f905d4d07261f4f29be410f57d2587e4
MaximkaStar <max.martsinkus@mail.ru>
2017-12-27 23:26:06 +0600

    garbage_collector

---
+++ garbage_collector/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace garbage_collector
+{
+    public class Item
+    {
+        public int Age = 10;
+        public int Date = 20022002;
+    }
+}
---
+++ garbage_collector/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.GC;
+
+namespace garbage_collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item peace = new Item();
+            GC.Collect(0, GCCollectionMode.Forced);
+        }
+    }
+}
